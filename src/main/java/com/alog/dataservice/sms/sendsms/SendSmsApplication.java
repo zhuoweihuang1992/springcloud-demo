71cbdb08ef4d96238c2f946c66d544cb7e630db1
huangzhuowei <zhuoweihuang@139.com>
2019-04-03 14:04:46 +0800

    demo

---
+++ src/main/java/com/alog/dataservice/sms/sendsms/SendSmsApplication.java
@@ -0,0 +1,13 @@
+package com.alog.dataservice.sms.sendsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendSmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alog/dataservice/sms/sendsms/SendSmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alog.dataservice.sms.sendsms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SendSmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
